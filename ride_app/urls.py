d8966a0ad1c8410f833bbd8ad3b376088305fc06
abynxv <abhinav.pk@hotmail.com>
2025-08-10 23:13:28 +0530

    Update: rider url

--- ride_app/urls.py
+++ ride_app/urls.py
@@ -4,7 +4,7 @@ from .all_views.rider_views import RiderViewSet
 from .all_views.driver_views import DriverRideViewSet
 
 router = DefaultRouter()
-router.register(r'rides', RiderViewSet, basename='rides')
+router.register(r'rider/rides', RiderViewSet, basename='rides')
 router.register(r'driver/rides', DriverRideViewSet, basename='driver-rides')
 
 urlpatterns = [
